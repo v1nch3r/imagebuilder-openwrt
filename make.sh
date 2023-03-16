b2e6432889a1df8bd79f4943ea9f81d10d701d5b
Vincher <127501499+vincherofficial@users.noreply.github.com>
2023-03-16 21:14:26 +0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ openwrt_dir="openwrt"
 imagebuilder_path="${make_path}/${openwrt_dir}"
 releases="21.02.1"
 targets="armvirt"
-imagebuilder_repo="https://downloads.immortalwrt.org/releases/${releases}/targets/${targets}/64/immortalwrt-imagebuilder-21.02.1-armvirt-64.Linux-x86_64.tar.xz"
+imagebuilder_repo="https://downloads.immortalwrt.org/releases/${releases}/targets/${targets}/64/immortalwrt-imagebuilder-${releases}-${targets}-64.Linux-x86_64.tar.xz"
 
 download_imagebuilder () {
     wget ${imagebuilder_repo}
