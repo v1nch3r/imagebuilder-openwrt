8808be210fa419779f769b53c62934175e2396e9
Vincher <127501499+vincherofficial@users.noreply.github.com>
2023-03-16 18:04:36 +0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-make_path="${pwd}"
+make_path="$(pwd)"
 openwrt_dir="openwrt"
 imagebuilder_path="${make_path}/${openwrt_dir}"
 releases="21.02.1"
@@ -7,8 +7,8 @@ processor="s905x"
 imagebuilder_repo="https://downloads.immortalwrt.org/releases/${releases}/targets/${targets}/64/immortalwrt-imagebuilder-21.02.1-armvirt-64.Linux-x86_64.tar.xz"
 my_packages="-luci-app-cpufreq -luci-app-turboacc -luci-app-filetransfer luci-theme-material luci-theme-argon luci-app-argon-config luci-app-ttyd luci-app-openclash luci-app-passwall luci-app-shutdown luci-app-netmonitor luci-app-zerotier nano htop openssh-sftp-server kmod-usb-net-cdc-ether usb-modeswitch comgt-ncm kmod-usb-net-huawei-cdc-ncm coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base"
 zero="immortalwrt-${releases}-${processor}.img"
-attach="${losetup -P -f}
-detach="${losetup -D}
+attach="$(losetup -P -f)
+detach="$(losetup -D)
 
 wget ${imagebuilder_repo}
 tar xf immortalwrt-imagebuilder-* && rm -r immortalwrt-imagebuilder-*.tar.xz
