59c677a299fa5ef4f5b2935efaec4b52549d77fe
Vincher <127501499+vincherofficial@users.noreply.github.com>
2023-03-17 08:59:08 +0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -22,7 +22,7 @@ luci_app_tinyfm="https://github.com/helmiau/helmiwrt-packages/releases/download/
 my_packages="-luci-app-cpufreq -luci-app-turboacc -luci-app-filetransfer luci-theme-material luci-theme-argon luci-app-argon-config luci-app-ttyd luci-app-openclash luci-app-passwall luci-app-shutdown luci-app-netmon luci-app-zerotier nano htop openssh-sftp-server kmod-usb-net-cdc-ether usb-modeswitch comgt-ncm kmod-usb-net-huawei-cdc-ncm coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base"
 
 # config img
-zero="immortalwrt-${releases}-${amlogic}.img
+zero="immortalwrt-${releases}-${amlogic}.img"
 
 # file system type
 bootfs="fat32"
