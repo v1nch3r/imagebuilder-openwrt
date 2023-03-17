dc5a2fc76a42ff16c3d3513f73a9114208ff8e5c
Vincher <127501499+vincherofficial@users.noreply.github.com>
2023-03-17 08:58:17 +0700

    Update make.sh

--- make.sh
+++ make.sh
@@ -9,6 +9,7 @@ imagebuilder_path="${make_path}/${openwrt_dir}"
 # targets
 releases="21.02.1"
 targets="armvirt"
+amlogic="s905x"
 
 # config repo
 imagebuilder_repo="https://downloads.immortalwrt.org/releases/${releases}/targets/${targets}/64/immortalwrt-imagebuilder-${releases}-${targets}-64.Linux-x86_64.tar.xz"
@@ -19,6 +20,13 @@ luci_app_tinyfm="https://github.com/helmiau/helmiwrt-packages/releases/download/
 
 # config package
 my_packages="-luci-app-cpufreq -luci-app-turboacc -luci-app-filetransfer luci-theme-material luci-theme-argon luci-app-argon-config luci-app-ttyd luci-app-openclash luci-app-passwall luci-app-shutdown luci-app-netmon luci-app-zerotier nano htop openssh-sftp-server kmod-usb-net-cdc-ether usb-modeswitch comgt-ncm kmod-usb-net-huawei-cdc-ncm coreutils-nohup bash iptables dnsmasq-full curl ca-certificates ipset ip-full iptables-mod-tproxy iptables-mod-extra libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip luci-compat luci luci-base"
+
+# config img
+zero="immortalwrt-${releases}-${amlogic}.img
+
+# file system type
+bootfs="fat32"
+rootfs="btrfs"
 
 download_imagebuilder () {
     wget ${imagebuilder_repo}
@@ -36,11 +44,21 @@ custom_packages () {
 build_rootfs () {
     cd ${imagebuilder_path}
     make image PROFILE="Default" PACKAGES="${my_packages}" FILES="files"
-    cd ${make_path} && ls -a
+    cd ${make_path}
+}
+
+build_img () {
+    dd if=/dev/zero of=${zero} bs=1M count=1500 status=progress
+    parted -s ${zero} mklabel msdos
+    parted -s ${zero} mkpart primary ${bootfs} 1 255
+    parted -s ${zero} mkpart primary ${rootfs} 255 1573
+    ls -a
+    
 }
 
 download_imagebuilder
 custom_packages
 build_rootfs
+build_img
 
 exit 0
