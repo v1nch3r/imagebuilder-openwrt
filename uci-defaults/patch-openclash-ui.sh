378118c2910eb5cf2e4630bf56f2f77f354ac7dd
Vincher <127501499+v1nch3r@users.noreply.github.com>
2023-03-19 19:10:29 +0700

    Update patch-openclash-ui.sh

--- uci-defaults/patch-openclash-ui.sh
+++ uci-defaults/patch-openclash-ui.sh
@@ -3,18 +3,18 @@
 clientui_path="/usr/lib/lua/luci/model/cbi/openclash/client.lua"
 
 patching1 () {
-    sed -i "s|d = SimpleForm("openclash")|-- d = SimpleForm("openclash")|g" ${clientui_path}
-    sed -i "s|d.title = translate("Credits")|-- d.title = translate("Credits")|g" ${clientui_path}
-    sed -i "s|d.reset = false|-- d.reset = false|g" ${clientui_path}
-    sed -i "s|d.submit = false|-- d.submit = false|g" ${clientui_path}
-    sed -i "s|d:section(SimpleSection).template  = "openclash/developer"|-- d:section(SimpleSection).template  = "openclash/developer"|g" ${clientui_path}
+    sed -i "131s|^|-- |" ${clientui_path}
+    sed -i "132s|^|-- |" ${clientui_path}
+    sed -i "133s|^|-- |" ${clientui_path}
+    sed -i "134s|^|-- |" ${clientui_path}
+    sed -i "135s|^|-- |" ${clientui_path}
 }
 
 patching2 () {
-    sed -i "s|dler = SimpleForm("openclash")|-- dler = SimpleForm("openclash")|g" ${clientui_path}
-    sed -i "s|dler.reset = false|-- dler.reset = false|g" ${clientui_path}
-    sed -i "s|dler.submit = false|-- dler.submit = false|g" ${clientui_path}
-    sed -i "s|dler:section(SimpleSection).template  = "openclash/dlercloud"|-- dler:section(SimpleSection).template  = "openclash/dlercloud"|g" ${clientui_path}
+    sed -i "137s|^|-- |" ${clientui_path}
+    sed -i "138s|^|-- |" ${clientui_path}
+    sed -i "139s|^|-- |" ${clientui_path}
+    sed -i "140s|^|-- |" ${clientui_path}
 }
 
 patch_done () {
